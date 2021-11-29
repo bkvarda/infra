566f45e4644fb21edd70836e96c3e8c31367f819
Brandon Kvarda <bjkvarda@gmail.com>
2021-11-29 13:38:27 -0800

    db workspace

--- aws/dev/databricks-dev-workspace/outputs.tf
+++ aws/dev/databricks-dev-workspace/outputs.tf
@@ -3,20 +3,6 @@ output "databricks_host" {
   value = databricks_mws_workspaces.this.workspace_url
 }
 
-// initialize provider in normal mode
-provider "databricks" {
-  // in normal scenario you won't have to give providers aliases
-  alias = "created_workspace"
-  host = databricks_mws_workspaces.this.workspace_url
-}
-
-// create PAT token to provision entities within workspace
-resource "databricks_token" "pat" {
-  provider = databricks.created_workspace
-  comment  = "Terraform Provisioning"
-  lifetime_seconds = 86400
-}
-
 // export token for integration tests to run on
 output "databricks_token" {
   value     = databricks_token.pat.token_value
