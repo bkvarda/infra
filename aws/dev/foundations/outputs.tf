41436fffb0d24d23113f00004e0f51a23a7c9e39
Brandon Kvarda <bjkvarda@gmail.com>
2021-12-06 10:14:31 -0800

    Updated outputs

--- aws/dev/foundations/outputs.tf
+++ aws/dev/foundations/outputs.tf
@@ -18,7 +18,7 @@ output "other_dev_vpc_private_subnets" {
     value = module.other_dev_vpc.private_subnets
 }
 
-output "dev_vpc_default_security_group_id" {
+output "other_dev_vpc_default_security_group_id" {
     value = [module.other_dev_vpc.default_security_group_id]
 }
 
