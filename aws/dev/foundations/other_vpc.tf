b548b9aa555df1fbfb14da1a53e19e16b4df30b2
Brandon Kvarda <bjkvarda@gmail.com>
2021-12-06 10:16:30 -0800

    Updated otehr vpc

--- aws/dev/foundations/other_vpc.tf
+++ aws/dev/foundations/other_vpc.tf
@@ -1,8 +1,8 @@
 locals {
-    region = "us-east-2"
+    other_region = "us-east-2"
 }
 
-data "aws_availability_zones" "available" {
+data "aws_availability_zones" "other_available" {
     state = "available"
 }
 
@@ -11,13 +11,13 @@ resource "aws_eip" "dev_nat_ip" {
     vpc = true
 }
 
-module "dev_vpc" {
+module "other_dev_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = var.vpc_name
+  name = var.other_vpc_name
   cidr = "10.0.0.0/16"
 
-  azs             = data.aws_availability_zones.available.names
+  azs             = data.aws_availability_zones.other_available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
