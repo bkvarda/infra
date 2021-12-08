8f3306427ec45b05b6459354cb70fc1a20b1602b
Brandon Kvarda <bjkvarda@gmail.com>
2021-12-08 15:13:47 -0800

    Instance profile

--- aws/dev/databricks-dev-workspace/workspace.tf
+++ aws/dev/databricks-dev-workspace/workspace.tf
@@ -138,3 +138,60 @@ resource "databricks_token" "pat" {
   comment  = "Terraform Provisioning"
   lifetime_seconds = 86400
 }
+
+// Instance profile
+data "aws_iam_policy_document" "assume_role_for_ec2" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+resource "aws_iam_role" "role_for_s3_access" {
+  name               = "shared-ec2-role-for-s3"
+  description        = "Role for shared access"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_for_ec2.json
+}
+data "aws_iam_policy_document" "pass_role_for_s3_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = [aws_iam_role.role_for_s3_access.arn]
+  }
+}
+resource "aws_iam_policy" "pass_role_for_s3_access" {
+  name   = "shared-pass-role-for-s3-access"
+  path   = "/"
+  policy = data.aws_iam_policy_document.pass_role_for_s3_access.json
+}
+resource "aws_iam_role_policy_attachment" "cross_account" {
+  policy_arn = aws_iam_policy.pass_role_for_s3_access.arn
+  role       = "${local.prefix}-crossaccount"
+}
+resource "aws_iam_instance_profile" "shared" {
+  name = "shared-instance-profile"
+  role = aws_iam_role.role_for_s3_access.name
+}
+resource "databricks_instance_profile" "shared" {
+  instance_profile_arn = aws_iam_instance_profile.shared.arn
+}
+resource "databricks_cluster" "this" {
+  cluster_name            = "Shared Autoscaling"
+  spark_version           = "6.6.x-scala2.11"
+  node_type_id            = "i3.xlarge"
+  autotermination_minutes = 20
+  autoscale {
+    min_workers = 1
+    max_workers = 50
+  }
+  aws_attributes {
+    instance_profile_arn    = databricks_instance_profile.shared.id
+    availability            = "SPOT"
+    zone_id                 = "us-east-1"
+    first_on_demand         = 1
+    spot_bid_price_percent  = 100
+  }
+}
